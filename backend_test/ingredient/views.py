a2bedb2df1e7e611b9fc8f79df3c681275017637
Thomas Bernard <thomashbernard@gmail.com>
2021-10-12 16:39:13 +0200

    [DEL] delete action on ingredients

--- backend_test/ingredient/views.py
+++ backend_test/ingredient/views.py
@@ -1,8 +1,13 @@
 from rest_framework import viewsets
+from rest_framework import mixins
 
 from .models import Ingredient
 from .serializers import IngredientSerializer
 
-class IngredientViewSet(viewsets.ModelViewSet):
+class IngredientViewSet(mixins.CreateModelMixin,
+                        mixins.ListModelMixin,
+                        mixins.UpdateModelMixin,
+                        mixins.RetrieveModelMixin,
+                        viewsets.GenericViewSet):
     queryset = Ingredient.objects.all()
     serializer_class = IngredientSerializer
